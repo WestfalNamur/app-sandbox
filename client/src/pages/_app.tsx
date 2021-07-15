4318af80008dc3d697a816184e712311b6740fbb
Stefan Crummenerl <crummenerls@gmail.com>
2021-07-15 17:01:25 +0200

    DEV CLIENT: Flexbox - Hello World

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import { Provider } from 'react-redux'
-import type { AppProps } from 'next/app'
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
 
-import store from '../app/store'
+import store from "../app/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
