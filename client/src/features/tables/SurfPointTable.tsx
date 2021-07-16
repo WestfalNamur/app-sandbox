7736080a93f102d8084394d32aa4de8a0ce49179
Stefan Crummenerl <crummenerls@gmail.com>
2021-07-16 09:54:29 +0200

    DEV CLIENT: Material UI - Typography and makeStyles

--- client/src/features/tables/SurfPointTable.tsx
+++ client/src/features/tables/SurfPointTable.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const userStyles = makeStyles({
+  btnStyle: {
+    fontStyle: "oblique",
+  },
+});
 
 export default function SurfPointTable() {
+  const classes = userStyles();
   return (
     <div>
-      <Button color="secondary">Hello Material</Button>
+      <Button color="primary">
+        <Typography className={classes.btnStyle}>
+          Hello, Button and Typography
+        </Typography>
+      </Button>
     </div>
   );
 }
--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,14 +1,14 @@
 import { createTheme } from "@material-ui/core/styles";
-import red from "@material-ui/core/colors/red";
+import { red, blueGrey, brown } from "@material-ui/core/colors";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: blueGrey[400],
     },
     secondary: {
-      main: "#FFFFFF",
+      main: brown[400],
     },
     error: {
       main: red.A400,
