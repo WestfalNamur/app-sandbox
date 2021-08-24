e2ea6d44750d121ce40234fe06a81c0e9fda1765
Stefan Crummenerl <crummenerls@gmail.com>
2021-08-24 20:39:13 +0200

    [CLN]: Rm unused old client

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,32 +1,53 @@
+// The redux store keeps track of all data in our client where the source of
+// truth is not outside of our client. Current view options, tokens etc. While
+// i.e Dashboard data for some service data would have their source of truth in
+// the respective service.
+
+import {
+  configureStore,
+  combineReducers,
+  ThunkAction,
+  Action,
+} from "@reduxjs/toolkit";
 import { logger } from "redux-logger";
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 
-import { gempyModelApi } from "../features/dataView/gempyDataApi";
-import { counterSlice } from "../features/counter/counterSlice";
+import counterReducer from "../features/counter/counterSlice";
+import { gempyModelTabularData } from "../features/gempyModelTable/gempyTabularDataApi";
 
-// Combine reducer
-// Api slices do not store data in a classical sense but are cached queries.
-const reducer = {
-  [gempyModelApi.reducerPath]: gempyModelApi.reducer,
-  counter: counterSlice,
-};
-
-// Collect middleware
-const middlewares = [gempyModelApi.middleware, logger];
-
-// Configure store
-export const store = configureStore({
-  reducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middlewares),
+// Combine our slice reducer
+const reducer = combineReducers({
+  // Source of truth is client
+  counter: counterReducer,
+  // Source of truth is web-api
+  [gempyModelTabularData.reducerPath]: gempyModelTabularData.reducer,
 });
 
-// Export Store-Types
+// Middleware
+// You should be able to pass multiple middleware to a single concat call as far
+// as I know.If for some reason that doesn't work, do several concat calls in a
+// row.– markerikson
+const middleware = (getDefaultMiddleware: any) =>
+  getDefaultMiddleware().concat(logger, gempyModelTabularData.middleware);
+
+// Create store and the makeStore function. This functions is used during
+// testing.
+export function makeStore() {
+  return configureStore({
+    reducer,
+    middleware,
+  });
+}
+const store = makeStore();
+
+// Types of our store, dispatches, thunks
+export type AppState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
-  RootState,
+  AppState,
   unknown,
   Action<string>
 >;
+
+// Export the store object
+export default store;
