d80dd2b04304e13f93f153a11434cb128ece4982
Stefan Crummenerl <crummenerls@gmail.com>
2021-08-27 21:47:34 +0200

    DEV CLINET [TST]: Hello, Jest

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import SurfPointTable from "./features/gempyData/SurfacePointTable";
 const App = (): ReactElement => {
   return (
     <div className="App">
+      <h1>Hello Jest!</h1>
       <SurfPointTable />
     </div>
   );
